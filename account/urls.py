0d7f9ccbb2707e3d0d5ce729fa4176bd4aca9107
aslan <matackubovaslan@gmail.com>
2024-07-25 19:09:10 +0500

    lox

---
+++ account/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from .views import dashboard, register
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeDoneView, PasswordChangeView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+
+urlpatterns = [
+    # path('login/', user_login, name='login')
+    # path('login/', LoginView.as_view(), name='login'),
+    # path('logout/', LogoutView.as_view(), name='logout'),
+    # path('password-change/', PasswordChangeView.as_view(), name='password_change'),
+    # path('password-change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
+    # path('password-reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('password-reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('password-reset/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('', include('django.contrib.auth.urls')),
+    path('', dashboard, name='dashboard'),
+    path('register/', register, name='register'),
+]
