1d418c63f56c8a0dda0729922100f4363b6da608
aslan <matackubovaslan@gmail.com>
2024-08-03 20:10:31 +0500

    booklet works

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'images',
+
     'social_django',
     'django_extensions',
 ]
@@ -139,7 +142,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
